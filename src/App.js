4e68b59328244386b688e5d3c1524fdb2d1875a8
Dafei Lu <dlu2@wpi.edu>
2019-06-04 16:39:45 -0400

    Added login and leader board

--- src/App.js
+++ src/App.js
@@ -2,10 +2,65 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
+import * as Auth0 from 'auth0-web';
+import io from 'socket.io-client';
+
+Auth0.configure({
+  domain: 'dafei.auth0.com',
+  clientID: '7gFVqRdN5kZ57wOmiLuzBEpHIR1X57Vt',
+  redirectUri: 'http://localhost:3000/',
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+  audience: 'https://aliens-go-home.digituz.com.br',
+});
 
 class App extends Component {
   componentDidMount() {
     const self = this;
+
+    Auth0.handleAuthCallback();
+
+    Auth0.subscribe((auth) => {
+      if (!auth) return;
+
+      const playerProfile = Auth0.getProfile();
+      const currentPlayer = {
+        id: playerProfile.sub,
+        maxScore: 0,
+        name: playerProfile.name,
+        picture: playerProfile.picture,
+      };
+
+      this.props.loggedIn(currentPlayer);
+
+      const socket = io('http://localhost:3001', {
+        query: `token=${Auth0.getAccessToken()}`,
+      });
+
+      let emitted = false;
+      socket.on('players', (players) => {
+        this.props.leaderboardLoaded(players);
+
+        if (emitted) return;
+        socket.emit('new-max-score', {
+          id: playerProfile.sub,
+          maxScore: 120,
+          name: playerProfile.name,
+          picture: playerProfile.picture,
+        });
+        emitted = true;
+        setTimeout(() => {
+          socket.emit('new-max-score', {
+            id: playerProfile.sub,
+            maxScore: 222,
+            name: playerProfile.name,
+            picture: playerProfile.picture,
+          });
+        }, 5000);
+      });
+    });
+
+
     setInterval(() => {
       self.props.moveObjects(self.canvasMousePosition);
     }, 10);
@@ -26,7 +81,9 @@ class App extends Component {
     return (
       <Canvas
         angle={this.props.angle}
+        currentPlayer={this.props.currentPlayer}
         gameState={this.props.gameState}
+        players={this.props.players}
         startGame={this.props.startGame}
         trackMouse={event => (this.trackMouse(event))}
       />
@@ -35,21 +92,26 @@ class App extends Component {
 }
 
 App.propTypes = {
-  angle: PropTypes.number.isRequired,
-  gameState: PropTypes.shape({
-    started: PropTypes.bool.isRequired,
-    kills: PropTypes.number.isRequired,
-    lives: PropTypes.number.isRequired,
-  }).isRequired,
-  flyingObjects: PropTypes.arrayOf(PropTypes.shape({
-    position: PropTypes.shape({
-      x: PropTypes.number.isRequired,
-      y: PropTypes.number.isRequired
-    }).isRequired,
-    id: PropTypes.number.isRequired,
-  })).isRequired,
-  moveObjects: PropTypes.func.isRequired,
-  startGame: PropTypes.func.isRequired,
+  // ... other propTypes definitions
+  currentPlayer: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  }),
+  leaderboardLoaded: PropTypes.func.isRequired,
+  loggedIn: PropTypes.func.isRequired,
+  players: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  })),
+};
+
+App.defaultProps = {
+  currentPlayer: null,
+  players: null,
 };
 
 export default App;
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,14 +4,29 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CannonBall from './CannonBall';
 import CurrentScore from './CurrentScore'
 import FlyingObject from './FlyingObject';
+import Heart from './Heart';
 import StartGame from './StartGame';
 import Title from './Title';
+import Login from './Login';
+import { signIn } from 'auth0-web';
+import Leaderboard from './Leaderboard';
 
 const Canvas = (props) => {
   const gameHeight = 1200;
   const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
+  const leaderboard = [
+    { id: 'd4', maxScore: 82, name: 'Ado Kukic', picture: 'https://twitter.com/KukicAdo/profile_image', },
+    { id: 'a1', maxScore: 235, name: 'Bruno Krebs', picture: 'https://twitter.com/brunoskrebs/profile_image', },
+    { id: 'c3', maxScore: 99, name: 'Diego Poza', picture: 'https://twitter.com/diegopoza/profile_image', },
+    { id: 'b2', maxScore: 129, name: 'Jeana Tahnk', picture: 'https://twitter.com/jeanatahnk/profile_image', },
+    { id: 'e5', maxScore: 34, name: 'Jenny Obrien', picture: 'https://twitter.com/jenny_obrien/profile_image', },
+    { id: 'f6', maxScore: 153, name: 'Kim Maida', picture: 'https://twitter.com/KimMaida/profile_image', },
+    { id: 'g7', maxScore: 55, name: 'Luke Oliff', picture: 'https://twitter.com/mroliff/profile_image', },
+    { id: 'h8', maxScore: 146, name: 'Sebastian Peyrott', picture: 'https://twitter.com/speyrott/profile_image', },
+  ];
   return (
     <svg
       id="aliens-go-home-canvas"
@@ -29,11 +44,13 @@ const Canvas = (props) => {
       <CannonPipe rotation={props.angle} />
       <CannonBase />
       <CurrentScore score={15} />
+      <Heart position={{x: -300, y:35}} />
 
       { ! props.gameState.started &&
       <g>
         <StartGame onClick={() => props.startGame()} />
         <Title />
+        <Leaderboard currentPlayer={leaderboard[6]} authenticate={signIn} leaderboard={leaderboard} />
       </g>
       }
 
